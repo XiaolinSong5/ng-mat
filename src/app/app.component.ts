4a887928da85bec17262ac1a0d188d9f005b4e10
Xiaolin <xiaolin.song@benerail.com>
2024-12-03 17:00:45 +0100

    filter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -62,4 +62,12 @@ export class AppComponent {
       });
 
   }
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
